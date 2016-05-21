96d677c06480f73ab5edf24bf8b7d39d3733a05a
JustMe <fenghebaiyang@gmail.com>
2016-05-21 12:36:24 +0800

    FileUploader

---
+++ app/src/main/java/com/main/androiddemo/utils/FileUploader.java
@@ -0,0 +1,273 @@
+package com.main.androiddemo.utils;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * @Description
+ * @Author xiaojianfeng
+ * @Date 16/5/19 下午8:40
+ */
+public class FileUploader {
+    private static final String LINE_FEED = "\r\n";
+    private final String boundary;
+    private HttpURLConnection httpConn;
+    private String charset;
+    private OutputStream outputStream;
+    private PrintWriter writer;
+
+    public FileUploader(String requestURL, String charset)
+            throws IOException {
+        this.charset = charset;
+
+        // creates a unique boundary based on time stamp
+        boundary = "===" + System.currentTimeMillis() + "===";
+
+        URL url = new URL(requestURL);
+        httpConn = (HttpURLConnection) url.openConnection();
+        httpConn.setUseCaches(false);
+        httpConn.setDoOutput(true); // indicates POST method
+        httpConn.setDoInput(true);
+        httpConn.setRequestProperty("Content-Type",
+                "multipart/form-data; boundary=" + boundary);
+        httpConn.setRequestProperty("User-Agent", "CodeJava Agent");
+        httpConn.setRequestProperty("Test", "Bonjour");
+        outputStream = httpConn.getOutputStream();
+        writer = new PrintWriter(new OutputStreamWriter(outputStream, charset),
+                true);
+    }
+
+    /**
+     * Adds a form field to the request
+     *
+     * @param name  field name
+     * @param value field value
+     */
+    public void addFormField(String name, String value) {
+        writer.append("--" + boundary).append(LINE_FEED);
+        writer.append("Content-Disposition: form-data; name=\"" + name + "\"")
+                .append(LINE_FEED);
+        writer.append("Content-Type: text/plain; charset=" + charset).append(
+                LINE_FEED);
+        writer.append(LINE_FEED);
+        writer.append(value).append(LINE_FEED);
+        writer.flush();
+    }
+
+    /**
+     * Adds a upload file section to the request
+     *
+     * @param fieldName  name attribute in <input type="file" name="..." />
+     * @param uploadFile a File to be uploaded
+     * @throws IOException
+     */
+    public void addFilePart(String fieldName, File uploadFile)
+            throws IOException {
+        String fileName = uploadFile.getName();
+        writer.append("--" + boundary).append(LINE_FEED);
+        writer.append(
+                "Content-Disposition: form-data; name=\"" + fieldName
+                        + "\"; filename=\"" + fileName + "\"")
+                .append(LINE_FEED);
+        writer.append(
+                "Content-Type: "
+                        + URLConnection.guessContentTypeFromName(fileName))
+                .append(LINE_FEED);
+        writer.append("Content-Transfer-Encoding: binary").append(LINE_FEED);
+        writer.append(LINE_FEED);
+        writer.flush();
+
+        FileInputStream inputStream = new FileInputStream(uploadFile);
+        byte[] buffer = new byte[4096];
+        int bytesRead = -1;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
+        }
+        outputStream.flush();
+        inputStream.close();
+
+        writer.append(LINE_FEED);
+        writer.flush();
+    }
+
+    /**
+     * Adds a header field to the request.
+     *
+     * @param name  - name of the header field
+     * @param value - value of the header field
+     */
+    public void addHeaderField(String name, String value) {
+        writer.append(name + ": " + value).append(LINE_FEED);
+        writer.flush();
+    }
+
+    /**
+     * Completes the request and receives response from the server.
+     *
+     * @return a list of Strings as response in case the server returned
+     * status OK, otherwise an exception is thrown.
+     * @throws IOException
+     */
+    public List<String> finish() throws IOException {
+        List<String> response = new ArrayList<String>();
+
+        writer.append(LINE_FEED).flush();
+        writer.append("--" + boundary + "--").append(LINE_FEED);
+        writer.close();
+
+        // checks server's status code first
+        int status = httpConn.getResponseCode();
+        if (status == HttpURLConnection.HTTP_OK) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    httpConn.getInputStream()));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                response.add(line);
+            }
+            reader.close();
+            httpConn.disconnect();
+        } else {
+            throw new IOException("Server returned non-OK status: " + status);
+        }
+
+        return response;
+    }
+
+
+    public static String newUploadFile(String path, HashMap<String, String> params, HashMap<String, File> files) {
+
+        String result = "";
+        String BOUNDARY = UUID.randomUUID().toString();  //边界标识   随机生成
+        String PREFIX = "--", LINE_END = "\r\n";
+        String CONTENT_TYPE = "multipart/form-data";   //内容类型
+
+        DataOutputStream dos = null;
+        BufferedInputStream input = null;
+        HttpURLConnection conn = null;
+        try {
+            URL url = new URL(path);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(20000);
+            conn.setConnectTimeout(20000);
+            conn.setDoInput(true);  //允许输入流
+            conn.setDoOutput(true); //允许输出流
+            conn.setUseCaches(false);  //不允许使用缓存
+            conn.setRequestMethod("POST");  //请求方式
+            conn.setRequestProperty("Charset", "utf-8");  //设置编码
+            conn.setRequestProperty("connection", "keep-alive");
+            conn.setRequestProperty("Content-Type", CONTENT_TYPE + ";boundary=" + BOUNDARY);
+
+            //取得输出流
+            dos = new DataOutputStream(conn.getOutputStream());
+
+            //写请求参数
+            if (params.size() > 0) {
+                Set<String> paramKeys = params.keySet();
+                Iterator<String> paramIterator = paramKeys.iterator();
+                StringBuffer sb = new StringBuffer();
+                while (paramIterator.hasNext()) {
+                    String key = paramIterator.next();
+
+                    sb.append(PREFIX);
+                    sb.append(BOUNDARY);
+                    sb.append(LINE_END);
+                    sb.append("Content-Disposition: form-data; name=\"" + key + "\"").append(LINE_END);
+                    sb.append("Content-Type: text/plain; charset=utf-8").append(LINE_END);
+                    sb.append(LINE_END);
+                    sb.append(params.get(key)).append(LINE_END);
+                }
+                dos.write(sb.toString().getBytes());
+            }
+            //写入文件
+            if (files.size() > 0) {
+                Set<String> fileKeys = files.keySet();
+                Iterator<String> fileIterator = fileKeys.iterator();
+                while (fileIterator.hasNext()) {
+
+                    StringBuffer sb = new StringBuffer();
+                    String fieldName = fileIterator.next();
+                    String fileName = files.get(fieldName).getName();
+                    sb.append(PREFIX).append(BOUNDARY).append(LINE_END);
+                    sb.append("Content-Disposition: form-data; name=\"" + fieldName + "\"; filename=\"" + fileName + "\"").append(LINE_END);
+                    sb.append("Content-Type: " + URLConnection.guessContentTypeFromName(fileName)).append(LINE_END);
+                    sb.append("Content-Transfer-Encoding: binary").append(LINE_END);
+                    sb.append(LINE_END);
+
+                    dos.write(sb.toString().getBytes());
+                    dos.flush();
+
+                    InputStream is = new FileInputStream(files.get(fieldName));
+                    byte[] bytes = new byte[1024];
+                    int len = 0;
+                    while ((len = is.read(bytes)) != -1) {
+                        dos.write(bytes, 0, len);
+                    }
+                    is.close();
+                    dos.write(LINE_END.getBytes());
+                    dos.flush();
+                }
+            }
+            //写入结束标志
+            byte[] end_data = (PREFIX + BOUNDARY + PREFIX + LINE_END).getBytes();
+            dos.write(end_data);
+            dos.flush();
+
+            /**
+             * 获取响应码  200=成功
+             * 当响应成功，获取响应的流
+             */
+            int code = conn.getResponseCode();
+            if (code == HttpURLConnection.HTTP_OK) { // 若响应码以2开头则读取响应头总的返回信息
+                input = new BufferedInputStream(conn.getInputStream());
+                StringBuffer sb = new StringBuffer();
+                int length = -1;
+                while ((length = input.read()) != -1) {
+                    sb.append(length);
+                }
+                result = sb.toString();
+            } else {
+                result = "error";
+            }
+            Logger.dd("result", result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (dos != null) {
+                try {
+                    dos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+        return result;
+    }
+}
