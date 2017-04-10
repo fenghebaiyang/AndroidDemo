61564f61b4f85f47836b3519018c4208d303d851
xiaojianfeng <fenghebaiyang@gmail.com>
2017-04-10 20:02:21 +0800

    下拉刷新控件选择

---
+++ app/src/main/java/com/main/androiddemo/activity/RefreshActivity.java
@@ -0,0 +1,37 @@
+package com.main.androiddemo.activity;
+
+import com.main.androiddemo.R;
+
+/**
+ * <br/> Description:
+ * <br/> Author: xiaojianfeng
+ * <br/> Version: 1.0
+ * <br/> Date: 2017/4/10 0010
+ */
+public class RefreshActivity extends BaseActivity {
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_refresh;
+    }
+
+    @Override
+    protected void findViews() {
+
+    }
+
+    @Override
+    protected void initGetData() {
+
+    }
+
+    @Override
+    protected void widgetListener() {
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
+}
